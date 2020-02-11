90afb1f6e4675331b88e0adfeee7ef6851a65910
Alex <alexcc17@gmail.com>
2020-02-11 12:39:15 -0500

    Setup Core Data classes

--- Salesforce Itunes/Core Data/Movies/Movie+Convenience.swift
+++ Salesforce Itunes/Core Data/Movies/Movie+Convenience.swift
@@ -7,3 +7,27 @@
 //
 
 import Foundation
+import CoreData
+
+extension Movies {
+    
+    convenience init(title: String, director: String, avatarUrl: String, context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+        self.init(context: context)
+        
+        self.title = title
+        self.director = director
+        self.avatarUrl = avatarUrl
+    }
+    
+    // creates Movie from MovieRepresentation
+    convenience init?(movieRepresentation: MovieRepresentation, context: NSManagedObjectContext = CoreDataStack.shared.mainContext) { // optional bc it may not pull data from backend
+        self.init(title: movieRepresentation.title, director: movieRepresentation.director, avatarUrl: movieRepresentation.avatarUrl, context: context)
+    }
+    
+    // converts Movie to MovieRepresentation before going to JSON
+    var movieRepresentation: MovieRepresentation? {
+        guard let title = title, let director = director, let avatarUrl = avatarUrl else {return nil}
+                
+        return MovieRepresentation(title: title, director: director, avatarUrl: avatarUrl)
+    }
+}
--- Salesforce Itunes/Core Data/Movies/MovieRepresentation.swift
+++ Salesforce Itunes/Core Data/Movies/MovieRepresentation.swift
@@ -7,3 +7,25 @@
 //
 
 import Foundation
+
+struct MovieRepresentation: Codable, Equatable {
+    
+    // this needs to exactly match our Firebase data
+    var title: String
+    var director: String
+    var avatarUrl: String
+}
+
+// we need to make sure our Firebase data matches our CoreData data
+
+func == (lhs: MovieRepresentation, rhs: Movies) -> Bool {
+    return lhs.title == rhs.title && lhs.director == rhs.director && lhs.avatarUrl == rhs.avatarUrl
+}
+
+func == (lhs: Movies, rhs: MovieRepresentation) -> Bool {
+    return rhs == lhs
+}
+
+func != (lhs: MovieRepresentation, rhs: Movies) -> Bool {
+    return !(rhs == lhs)
+}
